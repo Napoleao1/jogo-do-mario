156c0713fce6e781ece1620ac806b7fd79d4e0d4
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-30 20:36:36 -0300

    ajustei jump

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 const mario = document.querySelector('.mario')// selecionando  a classe do mario
 
 const jump = () => {
-    mario.classList.add('jump') // adicionando a classe jump para imagem "mario"
-}
+    mario.classList.add('jump'); // adicionando a classe jump para imagem "mario
+    
+    setTimeout(() => {
+    mario.classList.remove('jump');  
 
+    }, 500)
+}
 
 document.addEventListener('keydown', jump);
