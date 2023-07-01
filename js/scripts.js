e4920241c74d9e03288901518d57328e2c13a780
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-30 21:15:11 -0300

    configurando condições

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-const mario = document.querySelector('.mario')// selecionando  a classe do mario
+const mario = document.querySelector('.mario');// selecionando  a classe do mario
 
-const jump = () => {
+const jump = () => { 
     mario.classList.add('jump'); // adicionando a classe jump para imagem "mario
     
     setTimeout(() => {
@@ -8,5 +8,19 @@ const jump = () => {
 
     }, 500)
 }
+
+const pipe = document.querySelector('.pipe');
+
+const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px',''); // conversão de string para number
+    console.log(marioPosition);
+
+    if(pipePosition <= 120){
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+    }
+}, 10)
 
 document.addEventListener('keydown', jump);
