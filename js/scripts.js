26bb569e4123982d51e48d3c12bd813a6e510259
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-30 21:34:25 -0300

    finalizei estilizando as nuvens

--- js/scripts.js
+++ js/scripts.js
@@ -14,13 +14,25 @@ const pipe = document.querySelector('.pipe');
 const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px',''); // conversão de string para number
+
     console.log(marioPosition);
 
-    if(pipePosition <= 120){
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition <= 80){
 
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './images/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop);
     }
+
 }, 10)
+
 
 document.addEventListener('keydown', jump);
